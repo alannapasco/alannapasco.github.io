8817c95416b0c7ccce3d4cf2194f4782fef44e1f
lanarae <pasco.rae@gmail.com>
2024-02-10 23:13:49 -0500

    create reusable header; update index.html page to use new header component

---
+++ javascript/header.js
@@ -0,0 +1,43 @@
+
+// Contains header to be used on every page site-wide
+function createHeader() {
+    const header = document.createElement('header');
+    header.innerHTML = 
+    `
+        <style>
+        header {
+            padding-bottom:30px;
+        }
+          
+        nav {
+            display: inline-block;
+        }
+          
+        header #home_tab{
+            font-size: 30px;
+            text-align: left;
+            position: relative;
+            color:#363636;
+        }
+          
+        header #home_tab:hover{
+            color:rgb(46, 84, 103);
+        }
+          
+        header #right_tabs{ 
+            font-size: 25px;
+            float: right;
+            position: relative;
+            padding-top: 0.5%;
+        }
+
+        </style>
+        <a href="index.html"><nav id="home_tab">Alanna Pasco</nav></a>
+        <nav id="right_tabs">
+            <span class="tab"><a href="about.html" >About</a></span>
+            <span style="color:#363636">|</span>
+            <span class="tab"><a href="code.html" >Work</a></span>
+        </nav>
+    `;
+    return header;
+}
